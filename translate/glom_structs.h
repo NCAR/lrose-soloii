4903d2b8897ad08b411f2c351e7d1a1d028907fb
oye <oye@f31e7c6d-f41f-0410-9234-e4ad7f416ffa>
2002-07-03 14:55:13 +0000

    Added the FRIB struct to this file of structs use by ~/pl/crack_structs.pl to produce a swap table for this struct.
    git-svn-id: http://svn.eol.ucar.edu/svn/soloii/trunk@11 f31e7c6d-f41f-0410-9234-e4ad7f416ffa

--- translate/glom_structs.h
+++ translate/glom_structs.h
@@ -2607,3 +2607,78 @@ struct radar_d {
 }; /* End of Structure */
 
 
+
+struct field_radar_i {
+    char  field_radar_info[4];	/* Identifier for a field written */
+				/* radar information block */
+				/* (ascii characters FRIB). */
+    long field_radar_info_len;	/* Length of this field written radar */
+				/* information block in bytes. */
+    long data_sys_id;		/* Data system identification. */
+    float loss_out;		/* Waveguide Losses between Transmitter and */
+				/* antenna in db. */
+    float loss_in;		/* Waveguide Losses between antenna and Low */
+				/* noise amplifier in db. */
+    float loss_rjoint;		/* Losses in the rotary joint in db. */
+    float ant_v_dim;		/* Antenna Vertical Dimension in m. */
+    float ant_h_dim;		/* Antenna Horizontal Dimension in m. */
+    float ant_noise_temp;	/* Antenna Noise Temperature in degrees K. */
+    float r_noise_figure;	/* Receiver noise figure in dB*/
+    float xmit_power[5];        /* Nominal Peak transmitted power in dBm
+                                   by channel */
+    float x_band_gain;          /* X band gain in dB */
+    float receiver_gain[5];     /* Measured receiver gain in dB (by channel) */
+    float if_gain[5];           /* Measured IF gain in dB (by channel) */
+    float conversion_gain;      /* A to D conversion gain in dB */
+    float scale_factor[5];      /* Scale factor to account for differences in
+                                   the individual channels, and the inherent
+                                   gain due to summing over the dwell time */
+    float processor_const;      /* Constant used to scale dBz to
+				   units the display processors understand */
+    long dly_tube_antenna;	/* Time delay from RF being applied to
+				   tube and energy leaving antenna in ns. */
+    long dly_rndtrip_chip_atod;/* Time delay from a chip generated in the 
+				   timing module and the RF pulse entering
+                                   the A to D converters.  Need to take the
+                                   RF input to the HPA and inject it into 
+                                   the waveguide back at the LNA to make this
+                                   measurement  in ns*/
+    long dly_timmod_testpulse; /* Time delay from timing Module test
+				   pulse edge and test pulse arriving at
+				   the A/D converter in ns. */
+    long dly_modulator_on;	/* Modulator rise time (Time between
+				   video on into HPA and modulator full up in
+				   the high power amplifier) in ns. */
+    long dly_modulator_off;	/* Modulator fall time (Time between
+				   video off into the HPA
+				   and modulator full off) in ns. */
+    float peak_power_offset;     /*Added to the power meter reading of the
+                                   peak output power this yields actual
+				   peak output power (in dB) */ 
+    float test_pulse_offset;    /* Added to the power meter reading of the
+                                   test pulse this yields actual injected
+                                   test pulse power (dB) */
+    float E_plane_angle;           /* E-plane angle (tilt) this is the angle in
+				   the horizontal plane (when antennas are
+				   vertical) between a line normal to the
+				   aircraft's longitudinal axis and the radar
+				   beam in degrees.  Positive is in direction
+				   of motion (fore) */
+    float H_plane_angle;         /* H plane angle in degrees - this follows
+				    the sign convention described in the
+				    DORADE documentation for ROLL angle */
+    float encoder_antenna_up;   /* Encoder reading minus IRU roll angle
+				   when antenna is up and horizontal */
+    float pitch_antenna_up;     /* Antenna pitch angle (measured with
+				   transit) minus IRU pitch angle when
+				   antenna is pointing up */
+    short indepf_times_flg;	/* 0 = neither recorded, 1 = independent
+				   frequency data only, 3 = independent 
+				   frequency and time series data recorded */
+    short indep_freq_gate;	/* gate number where the independent frequency
+                                   data comes from */
+    short time_series_gate;	/* gate number where the time series data come
+                                   from */
+    short num_base_params;      /* Number of base parameters. */
+    char  file_name[80];	/* Name of this header file. */
+}; /* End of Structure */
