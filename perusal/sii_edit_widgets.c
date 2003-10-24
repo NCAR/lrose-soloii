feaef96fc38119df60220897bcf109a7727d21ca
oye <oye@f31e7c6d-f41f-0410-9234-e4ad7f416ffa>
2003-10-24 19:40:45 +0000

    Added code to implement storm motion removal.
    git-svn-id: http://svn.eol.ucar.edu/svn/soloii/trunk@199 f31e7c6d-f41f-0410-9234-e4ad7f416ffa

--- perusal/sii_edit_widgets.c
+++ perusal/sii_edit_widgets.c
@@ -2127,6 +2127,10 @@ void edit_cmds_help_widget (guint frame_num )
   sii_glom_strings (remove_ring, nn, gen_gs);
   put_edit_cmds_help ("remove-ring", gen_gs);
 
+  nn = sizeof (remove_storm_motion)/sizeof (char *);
+  sii_glom_strings (remove_storm_motion, nn, gen_gs);
+  put_edit_cmds_help ("remove-storm-motion", gen_gs);
+
   nn = sizeof (rewrite)/sizeof (char *);
   sii_glom_strings (rewrite, nn, gen_gs);
   put_edit_cmds_help ("rewrite", gen_gs);
