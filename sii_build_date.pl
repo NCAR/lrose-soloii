35ea4203d853da2006d5331a7cd0ee81670e6d64
burghart <burghart@f31e7c6d-f41f-0410-9234-e4ad7f416ffa>
2010-12-06 16:08:27 +0000

    Use a four-digit year when building the date stamp.
    git-svn-id: http://svn.eol.ucar.edu/svn/soloii/trunk@337 f31e7c6d-f41f-0410-9234-e4ad7f416ffa

--- sii_build_date.pl
+++ sii_build_date.pl
@@ -4,8 +4,8 @@
     = localtime (time);
 #print "$sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst\n";
 
-$tstamp = sprintf 'static char *sii_date_stamp = "%02d/%02d/%02d  %02d%02d";'
-    , ($mon+1,$mday,$year%100,$hour,$min);
+$tstamp = sprintf 'static char *sii_date_stamp = "%02d/%02d/%04d  %02d%02d";'
+    , ($mon+1,$mday,$year+1900,$hour,$min);
 
 print "$tstamp\n";
 
