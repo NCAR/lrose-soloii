91d5de81c9fc33999ed508c6da92f694ccf5df22
Erik Johnson <ej@ucar.edu>
2017-06-13 17:45:41 -0600

    VirtualBox VM: disable lockscreen; #2

--- packer/scripts/desktop.sh
+++ packer/scripts/desktop.sh
@@ -7,11 +7,17 @@ yum -y install xorg-x11-server-Xorg
 yum -y groupinstall 'GNOME Desktop Environment'
 yum -y install gdm
 
+#
+# set runlevel to 5, to boot to GDM
+#
 sed --in-place -e 's/id:3:initdefault/id:5:initdefault/' /etc/inittab
 
+#
+# configure GDM to automatically log in user vagrant
+#
 mv /tmp/custom.conf /etc/gdm/
 
-# sed --in-place -e 's/[daemon]/[daemon]\
-# AutomaticLoginEnable=true\
-# AutomaticLogin=vagrant\
-# /' /etc/gdm/custom.conf
+#
+# disable lockscreen
+#
+gconftool-2 --set /schemas/desktop/gnome/lockdown/disable_lock_screen --type boolean true
