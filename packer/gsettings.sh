b1711a3078d3e8f4d58ce35696fee1e1bdca7978
Erik Johnson <ej@ucar.edu>
2017-06-26 12:03:51 -0600

    Virtual Appliance: fix path to gsettings.sh from autostart file, log gsettings.sh status, make autostart file user specific; #2

--- packer/gsettings.sh
+++ packer/gsettings.sh
@@ -5,16 +5,19 @@
 #
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 
 #
 # disable cursor blinking
 #
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$(gsettings get org.gnome.Terminal.ProfilesList default|tr -d \')/ cursor-blink-mode off
 
-rm -f ~/.config/autostart/gsettings.desktop
-
 #
 # clean up desktop icons
 #
 gsettings set org.gnome.nautilus.desktop home-icon-visible false
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
+
+rm -f ~/.config/autostart/gsettings.desktop
+
+echo `date`: gsettings set, autostart file removed  >> ~/.gsettings.out
